195024568873c8d15aa82c81978af5b31c9b37a8
Kafi09 <kaficsmy9@gmail.com>
2022-11-28 14:36:05 +0600

    add middleware

--- index.js
+++ index.js
@@ -4,3 +4,10 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
