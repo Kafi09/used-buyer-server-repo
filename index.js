7b8c6b6cf3a03eb33bdebbf152e18b48fd13b788
Kafi09 <kaficsmy9@gmail.com>
2022-11-28 14:41:59 +0600

    add func

--- index.js
+++ index.js
@@ -14,6 +14,14 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cighgcw.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function run() {
+    try {
+        
+    }
+    finally {
+
+    }
+}
 
 
 
